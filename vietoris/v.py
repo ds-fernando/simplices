65803b91aa935e5dd6f8c70bc21615abd05a4363
Fernando León <fernandoleonsierra@gmail.com>
2023-10-20 15:05:35 -0500

    construcción complejo

--- vietoris/v.py
+++ vietoris/v.py
@@ -2,6 +2,8 @@ import math
 from random import random
 from matplotlib import pyplot
 import time
+import numpy
+from itertools import combinations
 
 def generate_circle(n, radius, max_noise):
     points = []
@@ -24,3 +26,46 @@ def plot_points(points, figure=None, radius=0, color='r'):
         for circle in [pyplot.Circle(point, radius, color=color)
                        for point in points]:
             axes.add_artist(circle)
+def get_points(points, indices):
+
+    return [points[index] for index in indices]
+
+def draw_triangle(triangle):
+    p1, p2, p3 = triangle
+    pyplot.plot([p1[0], p2[0]],[p1[1],p2[1]])
+    pyplot.plot([p1[0], p3[0]],[p1[1],p3[1]])
+    pyplot.plot([p2[0], p3[0]],[p2[1],p3[1]])
+
+def draw_line(line):
+    p1, p2 = line
+    pyplot.plot([p1[0], p2[0]],[p1[1],p2[1]])
+
+def draw_point(point):
+    pyplot.plot(point)
+
+def draw_simplicial_complex(simplices, points):
+    handlers = [draw_point, draw_line, draw_triangle]
+    for simplex in simplices:
+        handlers[len(simplex)-1](get_points(points, simplex))
+
+radius = 0.3
+circle = generate_circle(70, 3, radius)
+plot_points(circle)
+
+array_circle = numpy.array(circle)
+
+P = [[0,0], [1,0], [0,1]]
+A = numpy.array(P, dtype=float)
+
+
+[list(subset) for subset in combinations(P, 2)]
+twoskeleton = []
+threeskeleton = []
+
+for subset in combinations(P,2):
+    if math.dist(subset[0],subset[1])<=2:
+        twoskeleton.append(subset)
+
+for subset in combinations(P,3):
+    if math.dist(subset[0],subset[1])<=2 and math.dist(subset[1],subset[2])<=2 and math.dist(subset[0],subset[2])<=2:
+        threeskeleton.append(subset)
